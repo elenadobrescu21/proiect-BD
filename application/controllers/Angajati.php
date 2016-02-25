fb75a181db598cd8a49f947a79b2fbb06c642d88
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-02-25 21:02:28 +0200

    Update Angajati.php

--- application/controllers/Angajati.php
+++ application/controllers/Angajati.php
@@ -2,62 +2,50 @@
  
 class Angajati extends CI_Controller {
  
- function __construct()
- {
+ function __construct() {
    parent::__construct();
    $this->load->model(array('model_employee', 'model_departament', 'model_serviciu'));
  }
  
- function index()
- {
-	
-	//$data['dt'] = $this->model_employee->get_all_employees();
-	$data['ang_dep'] = $this->model_employee->angajat_departament();
-	$data['ang'] = $this->model_employee->count_all_employees();
-    $this->load->view('lista_angajati', $data);
+ function index() {
+   $data['ang_dep'] = $this->model_employee->angajat_departament();
+   $data['ang'] = $this->model_employee->count_all_employees();
+   $this->load->view('lista_angajati', $data);
  }
  
   function show_angajat_id() {
-		$id = $this->uri->segment(3);
-		$data['angajati'] = $this->model_employee->get_all_employees();
-		$data['single_angajat'] = $this->model_employee->show_angajat_id($id);
-		$this->load->view('adauga_info_aditionale', $data);
+    $id = $this->uri->segment(3);
+    $data['angajati'] = $this->model_employee->get_all_employees();
+    $data['single_angajat'] = $this->model_employee->show_angajat_id($id);
+    $this->load->view('adauga_info_aditionale', $data);
   }
   
   
-function adauga_info_aditionale() {
-	
-		$config['upload_path'] = "./images/";
-		$config['allowed_types'] = 'jpg|jpeg|gif|png';
-		$config['encrypt_name'] = true;
-		$filename = 'poza_angajat';
-		$this->load->library('upload', $config);
-		$id= $this->input->post('did');
-		$msg ="";
-		
-
-		$fields = array(
-					'Id_angajat'=> $id,
-					'studii_angajat' => $this->input->post('studii_angajat'),
-					'descriere_angajat' => $this->input->post('descriere_angajat'),
-					'link_facebook' => $this->input->post('link_facebook'));
+  function adauga_info_aditionale() {
+    $config['upload_path'] = "./images/";
+    $config['allowed_types'] = 'jpg|jpeg|gif|png';
+    $config['encrypt_name'] = true;
+    $filename = 'poza_angajat';
+    $this->load->library('upload', $config);
+    $id= $this->input->post('did');
+    $msg ="";
+    $fields = array(
+	     'Id_angajat'=> $id,
+	     'studii_angajat' => $this->input->post('studii_angajat'),
+	     'descriere_angajat' => $this->input->post('descriere_angajat'),
+	      'link_facebook' => $this->input->post('link_facebook'));
 	
 	
-		if(!$this->upload->do_upload($filename)) {
-			$msg = "error";
+    if(!$this->upload->do_upload($filename)) {
+      $msg = "error";
 			
-		}
-		else {
-			$data = $this->upload->data();
-			$this->model_employee->insert_info_aditionale($data['file_name'], $fields);
-			redirect('Admin');
-		}
-		
-				
-			
-		}
-
- 
+    }
+    else {
+      $data = $this->upload->data();
+      $this->model_employee->insert_info_aditionale($data['file_name'], $fields);
+      redirect('Admin');
+  }
+}
 }
  
 ?>
