6c8f3841ac33b78cc7745951b4dc2122a958b4aa
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-02-25 21:09:47 +0200

    Update Clienti.php

--- application/controllers/Clienti.php
+++ application/controllers/Clienti.php
@@ -2,33 +2,25 @@
  
 class Clienti extends CI_Controller {
  
- function __construct()
- {
+ function __construct() {
    parent::__construct();
    $this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
  }
  
- function index()
- {
-	
-	$data['clients'] = $this->model_user->get_all_clients();
-	$data['TotalClienti'] = $this->model_user->count_all_clients();
-	$data['MaxComenzi'] = $this->model_user->clienti_cu_cele_mai_multe_comenzi();
+ function index()  {
+    $data['clients'] = $this->model_user->get_all_clients();
+    $data['TotalClienti'] = $this->model_user->count_all_clients();
+    $data['MaxComenzi'] = $this->model_user->clienti_cu_cele_mai_multe_comenzi();
     $this->load->view('lista_clienti', $data);
  }
  
-  	function get_clienti($numar_comenzi){
-		
-		
-        $this->load->model('model_user','', TRUE);    
-		 $this->output
-           ->set_content_type("application/json")
-           ->set_output(json_encode($this->model_user->clienti_x_comenzi($numar_comenzi)));
+  function get_clienti($numar_comenzi){
+     $this->load->model('model_user','', TRUE);    
+     $this->output
+          ->set_content_type("application/json")
+          ->set_output(json_encode($this->model_user->clienti_x_comenzi($numar_comenzi)));
     
     } 	
- 
-
- 
 }
  
 ?>
