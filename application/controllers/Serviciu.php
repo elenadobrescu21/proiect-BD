91fa03f2179afa7eb5a0d75a0676075d86e6f77a
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-03-02 16:53:59 +0200

    Update Serviciu.php

--- application/controllers/Serviciu.php
+++ application/controllers/Serviciu.php
@@ -3,40 +3,29 @@
 class Serviciu extends CI_Controller {
 	
 	public function __construct() {
-		parent::__construct();
-		
-		$this->load->model(array('model_employee', 'model_departament', 'model_serviciu'));
-		
+	  parent::__construct();
+	  $this->load->model(array('model_employee', 'model_departament', 'model_serviciu'));
 	}
 	
 	public function lista_servicii() {
-		$data['serv'] = $this->model_serviciu->serviciu_departament();
-		$this->load->view('lista_servicii', $data);
+	  $data['serv'] = $this->model_serviciu->serviciu_departament();
+	  $this->load->view('lista_servicii', $data);
 	}
 	
 	public function index () {
-		
-		$data['departament'] = $this->model_departament->get_all_departments();
-		$this->load->view('adauga_serviciu', $data);
-		$Nume_serviciu = NULL;
-		$id_departament = NULL;
-		$Pret = NULL;
-		$submit = NULL;
-		extract($_POST);		
-		$params['Nume_serviciu'] = $Nume_serviciu;
-		
-		$params['Pret'] = $Pret;
-		$params['id_departament'] = $id_departament;
-		
-	
-		if(isset($submit) )
-		{
-			//insert update table here
-			$this->model_serviciu->insert_serviciu($params);
-			redirect('Serviciu/lista_servicii');
-		}
-		//$this->load->view('adauga_departament');
+	  $data['departament'] = $this->model_departament->get_all_departments();
+	  $this->load->view('adauga_serviciu', $data);
+	  $Nume_serviciu = NULL;
+	  $id_departament = NULL;
+	  $Pret = NULL;
+	  $submit = NULL;
+	  extract($_POST);		
+	  $params['Nume_serviciu'] = $Nume_serviciu;
+	  $params['Pret'] = $Pret;
+	  $params['id_departament'] = $id_departament;
+	  if(isset($submit) ) {
+	     $this->model_serviciu->insert_serviciu($params);
+	     redirect('Serviciu/lista_servicii');
+	  }
 	}
-	
-
 }
