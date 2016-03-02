d9af9164fbcac6621cfaf6467148dd93a5318c43
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-03-02 17:06:07 +0200

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,13 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
-		$data['deps'] = $this->model_departament->get_all_departments();
-
-		$this->load->view('front_page', $data);
+	public function index() {
+	  $this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
+	  $data['deps'] = $this->model_departament->get_all_departments();
+          $this->load->view('front_page', $data);
 	}
-	
-
 }
