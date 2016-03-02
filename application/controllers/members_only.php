0288a86d2a1f35934efb8fa8c8b9657a86923fa2
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-03-02 17:18:19 +0200

    Update members_only.php

--- application/controllers/members_only.php
+++ application/controllers/members_only.php
@@ -3,16 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Members_only extends CI_Controller {
 	
-		public function __construct() {
-		parent::__construct();
-		
-		$this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
+	public function __construct() {
+	  parent::__construct();
+	  $this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
 		
 	}
 
-	public function index()
-	{
-		$this->load->view('members_only');
+	public function index() {
+	  $this->load->view('members_only');
 	}
 	
 }
