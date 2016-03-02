de04dac69b7b0e8c9b0d4a8e79cb0be638ade540
Elena Dobrescu <elena.dobrescu@cnmv.ro>
2016-03-02 16:40:02 +0200

    Update Lista_servicii.php

--- application/controllers/Lista_servicii.php
+++ application/controllers/Lista_servicii.php
@@ -3,27 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Lista_servicii extends CI_Controller {
 	
-		public function __construct() {
-		parent::__construct();
-		
-		$this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
-		
+	public function __construct() {
+	  parent::__construct();
+	  $this->load->model(array('model_employee', 'model_departament', 'model_serviciu', 'model_user'));
 	}
 
-	public function index()
-	{
-		$data['deps'] = $this->model_departament->get_all_departments(); 
-		$this->load->view('servicii_home', $data);
+	public function index() {
+	  $data['deps'] = $this->model_departament->get_all_departments(); 
+	  $this->load->view('servicii_home', $data);
 	}
 	
-	function get_services($departament){
-		
-		$departament_id = $departament;
-        $this->load->model('Model_serviciu','', TRUE);    
+	function get_services($departament) {
+	  $departament_id = $departament;
+          $this->load->model('Model_serviciu','', TRUE);    
 		 $this->output
-           ->set_content_type("application/json")
-           ->set_output(json_encode($this->Model_serviciu->get_servicii_by_departament($departament_id)));
-
+                 ->set_content_type("application/json")
+                 ->set_output(json_encode($this->Model_serviciu->get_servicii_by_departament($departament_id)));
     } 	
 	
 }
